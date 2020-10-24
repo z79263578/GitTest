7f0286aa420c6b0a9b3fda53d96ba9388e7ebb99
仲瑋 林 <z79263578@gmail.com>
2020-10-24 21:20:16 +0800

    HI

--- CoreTest/CoreTest/Startup.cs
+++ CoreTest/CoreTest/Startup.cs
@@ -23,7 +23,7 @@ namespace CoreTest
             //789
             //TEST123456
             _Config = config;
-        }
+        }//TEST
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
